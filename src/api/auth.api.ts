3c26b747c61a1ae276c03fb7b44e19398e414640
josiah <adeagbojosiah1@gmail.com>
2025-06-27 17:06:51 +0100

    updated

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000'; // Replace with your actual backend base URL
+const API_BASE_URL = 'https://wealthwise-api.onrender.com'; // Replace with your actual backend base URL
 
 export const signupInstructor = async (formData: {
   firstName: string;
